117b4c3e2bbf7d72e93045922d26dbe50df9404f
jill64 <intents.turrets0h@icloud.com>
2023-12-07 23:33:49 +0900

    minor: conditional exports to optional

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { readable } from 'svelte/store'
 import { Listen } from './types/Listen.js'
 
-export declare const listen: Listen
+export const listen: Listen = <T>(key: string, fallback?: T) => {
+  const browser = typeof window !== 'undefined'
+  return readable(browser ? matchMedia(key).matches : fallback, (set) => {
+    if (browser) {
+      matchMedia(key).addEventListener('change', (e) => set(e.matches))
+    }
+  })
+}
