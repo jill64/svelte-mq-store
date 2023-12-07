0973321733c4c088399f29b23c5fe3da616d76a9
jill64 <intents.turrets0h@icloud.com>
2023-12-08 00:32:35 +0900

    fix: feature detection

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { readable } from 'svelte/store'
 import { Listen } from './types/Listen.js'
 
 export const listen: Listen = <T>(key: string, fallback?: T) => {
-  const browser = typeof window !== 'undefined'
-  return readable(browser ? matchMedia(key).matches : fallback, (set) => {
-    if (browser) {
+  const available = typeof matchMedia !== 'undefined'
+  return readable(available ? matchMedia(key).matches : fallback, (set) => {
+    if (available) {
       matchMedia(key).addEventListener('change', (e) => set(e.matches))
     }
   })
