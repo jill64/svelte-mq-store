8c0a5865a552ee12c6afe71389a636d1ff66151b
jill64 <intents.turrets0h@icloud.com>
2024-02-06 16:46:29 +0900

    refactor tests

--- tests/light.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('light', async ({ page }) => {
-  await page.emulateMedia({ colorScheme: 'light' })
-  await page.goto('/')
-  await expect(page.getByText('Device is light mode')).toBeVisible()
-})
