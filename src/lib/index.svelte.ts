2b16e6dd4912a98aef6acf6bfff1fa71aa8bfd5c
jill64 <contact@jill64.dev>
2024-07-24 23:00:51 +0900

    fix: type def

--- src/lib/index.svelte.ts
+++ src/lib/index.svelte.ts
@@ -1,6 +1,9 @@
 import { BROWSER } from 'esm-env'
 
-export const listen = <T = undefined>(key: string, fallback?: T) => {
+export const listen = <T = undefined>(
+  key: string,
+  fallback?: T
+): boolean | T | undefined => {
   let value = $state(BROWSER ? matchMedia(key).matches : fallback)
 
   if (BROWSER) {
